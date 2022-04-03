6e9277deaba5788abedc635f4cb80b388740048b
Daniel Mendez <soliddan211@gmail.com>
2022-04-02 23:47:35 -0400

    added code to user-controller.js

---
+++ controllers/user-controller.js
@@ -0,0 +1,123 @@
+const { User, Thought } = require("../models");
+
+// GET all users
+const userController = {
+  getAllUsers(req, res) {
+    User.find({})
+      .select("-__v")
+      .sort({ createdAt: "desc" })
+      .then((userData) => res.json(userData))
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+
+  // GET one user
+  getOneUser({ params }, res) {
+    User.findOne({ _id: params.id })
+      .populate({ path: "thoughts", select: "-__v" })
+      .populate({ path: "friends", select: "-__v" })
+      .select("-__v")
+      .sort({ createdAt: "desc" })
+      .then((userData) => {
+        if (!userData) {
+          res.status(404).json({ message: "No user found with that ID." });
+          return;
+        }
+        res.json(userData);
+      });
+  },
+
+  // POST a new user
+  createUser({ body }, res) {
+    User.create(body)
+      .then((userData) => {
+        res.json(userData);
+      })
+      .catch((err) => res.status(400).json(err));
+  },
+
+  // POST add a friend to a users friend list
+  addFriend({ params }, res) {
+    User.findOneAndUpdate(
+      { _id: params.userId },
+      { $push: { friends: params.friendId } },
+      { new: true, runValidators: true }
+    )
+      .select("-__v")
+      .then((userData) => {
+        if (!userData) {
+          res.status(404).json({ message: "No user found with that ID." });
+          return;
+        }
+        res.json({
+          message: `The user was added to your friend list.`,
+          userData,
+        });
+      })
+      .catch((err) => res.status(400).json(err));
+  },
+
+  //PUT a update to a user
+  updateUser({ params, body }, res) {
+    User.findOneAndUpdate({ _id: params.id }, body, {
+      new: true,
+      runValidators: true,
+    })
+      .select("-__v")
+      .then((userData) => {
+        if (!userData) {
+          res.status(404).json({ message: "No user found with that ID." });
+          return;
+        }
+        res.json({ message: "The user was updated.", userData });
+      })
+      .catch((err) => res.status(400).json(err));
+  },
+
+  // DELETE a user and Thoughts
+  deleteUser({ params }, res) {
+    User.findOneAndDelete({ _id: params.id })
+      .select("-__v")
+      .then((userData) => {
+        if (!userData) {
+          res.status(404).json({ message: "No user found with that ID." });
+        }
+        return Thought.deleteMany(
+          { username: userData.username },
+          { new: true, runValidators: true }
+        ).then((thoughtData) => {
+          res.json({
+            message: `The user ${userData.username} was deleted.`,
+            userData,
+            thoughtData,
+          });
+        });
+      })
+      .catch((err) => res.status(400).json(err));
+  },
+
+  // DELETE a firend from a friend list
+  removeFriend({ params }, res) {
+    User.findOneAndUpdate(
+      { _id: params.userId },
+      { $pull: { friends: params.friendId } },
+      { new: true, runValidators: true }
+    )
+      .select("-__v")
+      .then((userData) => {
+        if (!userData) {
+          res.status(404).json({ message: "No user found with that ID." });
+          return;
+        }
+        res.json({
+          message: "That user was removed from your friend list.",
+          userData,
+        });
+      })
+      .catch((err) => res.status(400).json(err));
+  },
+};
+
+module.exports = userController;
